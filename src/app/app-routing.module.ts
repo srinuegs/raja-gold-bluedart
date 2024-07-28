65b5f6b607d5a9ab0906a2a32748dde97f91ec8f
vjswaroop2 <swaroopvegi11@gmail.com>
2024-07-28 22:06:47 +0530

    first-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { PagenotfoundComponent } from './Components/pagenotfound/pagenotfound.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { BlogComponent } from './Components/blog/blog.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: '**', component: PagenotfoundComponent},
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component'
+import { AboutComponent } from './Components/about/about.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { LoginComponent } from './Components/login/login.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { BlogComponent } from './Components/blog/blog.component';
+import { PagenotfoundComponent } from './Components/pagenotfound/pagenotfound.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductsComponent,
+    LoginComponent,
+    SignupComponent,
+    BlogComponent,
+    PagenotfoundComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
